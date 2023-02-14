ec536130dffa70819ddd4931e2d2a2ca7f3659a3
aravindaan-110 <aravindaan.s@gmail.com>
2023-02-14 23:23:39 +0530

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,7 +10,7 @@ function onSubmit(event) {
   
   const script1 = document.createElement("script");
   script1.type = "text/javascript";
-  script1.id = "pap_x2s6df8d"
+  script1.id = "pap_x2s6df8d";
   script1.src = "https://binocs.postaffiliatepro.com/scripts/jwk8n1";
   
   
